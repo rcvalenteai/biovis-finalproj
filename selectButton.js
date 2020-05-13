53fe5d610038b77c06c8ed42ce09edeac59330bb
aspielman1 <aspielma@gmail.com>
2020-05-13 15:11:06 -0400

    bubble new
    bubble new

---
+++ selectButton.js
@@ -0,0 +1,9 @@
+allGroup = ["Celery", "Tomato"]
+
+d3.select("#ingredient_select")
+    .selectAll('myOptions')
+    .data(allGroup)
+    .enter()
+    .append('option')
+    .text(function (d) { return d; }) // text showed in the menu
+    .attr("value", function (d) { return d; }) // corresponding value returned by the button
